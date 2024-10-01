233f4753e3fb36501af795deb258c141a1c983b3
Andre Sato <andre.kubagawa@usp.br>
2024-10-01 14:30:13 -0300

    Remove core functions and link library, so that only the GUI code remains

--- db.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <optional>
-
-std::optional<int> getCountMarkedFeatures(const std::string &db_path);
-
-std::optional<std::map<std::string, int>> getCountByMarkedFeaturesType(const std::string &db_path);
